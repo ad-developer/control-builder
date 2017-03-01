12cae75891f48525e091edb401c04c8fc803d414
ad-developer <BusinessSubscription@gmail.com>
2017-03-01 16:49:08 -0700

    Create model.js

---
+++ js/model.js
@@ -0,0 +1,12 @@
+  [
+    {c: 'ad-text', id:'name'},
+    {c:'ad-div', "class": 'row',
+      cs:[
+        {c:: 'ad-div', "class": "col1", cs:[
+          {c:"ad-text"},{c:"ad-p-picker"}
+        ]},
+        {c: 'ad-div', "class": "col2", cs:[
+          {c:"ad-text"},{c:"ad-p-picker"}
+        ]}
+    ]}
+  ]
